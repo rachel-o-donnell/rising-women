a1fe767a3b19110b43a4cca40c923496f20826bd
IvetteMcDermott <ivettedemcdermott@gmail.com>
2023-03-16 21:43:37 +0000

    Add mentorsApp models to admin

--- mentors/admin.py
+++ mentors/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Mentor, Category, Subcategory
 
 # Register your models here.
+
+
+@admin.register(Mentor)
+class Mentor(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ('category', 'subcategory')
+    search_fields = ['category', 'subcategory',]
+    list_display = ('name', 'verified', 'bio', 'image',
+                    'website', 'linkin', 'category',
+                    'joined',)
+    list_editable = ['verified']
+
+
+@admin.register(Category)
+class Mentor(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+@admin.register(Subcategory)
+class Mentor(admin.ModelAdmin):
+    list_display = ('name',)
