58a131d59857fc3c9f018c53f56a13a0e187e0ae
rachel-o-donnell <hellotherachelo@gmail.com>
2023-03-16 00:53:15 +0000

    Install and connect allauth
    Added to requirements.txt, settings.py and connected urls.py

--- risingwomen/settings.py
+++ risingwomen/settings.py
@@ -41,9 +41,18 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django.contrib.staticfiles',
     'mentors',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
