ce044a266df8a4ff1e99e8fd0c7cefb54a67f4c9
IvetteMcDermott <ivettedemcdermott@gmail.com>
2023-03-17 22:13:26 +0000

    Modify in Mentor App, models, and urls

--- mentors/urls.py
+++ mentors/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.MentorsList.as_view(), name='mentorslist'),
-    path('<slug:slug>', views.MentorDetail.as_view(), name='mentordetail'),
+    path('', views.MentorsList.as_view(), name='mentors-list'),
+    path('<slug:slug>', views.MentorDetail.as_view(), name='mentor-detail'),
+    path('mentorapplication/', views.MentorApplicationForm.as_view(),
+         name='mentor-application'),
+    path('mentorapplication/thanks/', views.thankPage, name='thank-message'),
 ]
