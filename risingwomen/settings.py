993a7e059c6bb1d000b0898d78b4c78e691168dd
rachel-o-donnell <hellotherachelo@gmail.com>
2023-03-17 21:45:13 +0000

    Fix typo in settings

--- risingwomen/settings.py
+++ risingwomen/settings.py
@@ -184,7 +184,7 @@ else:
     EMAIL_BACKEND = 'django.core.mail.backends.stmp.EmailBackend'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST = smtp.gmail.com
+    EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_HOST_USER = os.environ.get('EMAIL_HOST_USER')
