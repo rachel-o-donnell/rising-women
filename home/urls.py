e15f90eeb3fad369d11a14c18c30a75375d16687
IvetteMcDermott <ivettedemcdermott@gmail.com>
2023-03-19 12:36:56 +0000

    Add about Us html, view and url in Home app

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('about-us', views.aboutUs, name='about-us'),
 ]
