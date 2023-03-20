e9f0ffc2cb7b23a01f60fae6d551d7fd65c09ebe
rachel-o-donnell <hellotherachelo@gmail.com>
2023-03-20 11:08:06 +0000

    Debug set to false and summernote x-frame
    settings.py

--- risingwomen/settings.py
+++ risingwomen/settings.py
@@ -28,7 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['rising-women.herokuapp.com', 'localhost']
 
